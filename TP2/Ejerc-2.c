bc5c7b35baf5c3d33802e5aa1d334e45ae0bb12f
MendioX <82616448+MendioX@users.noreply.github.com>
2023-09-29 17:07:46 -0300

    fix 2 functions

--- TP2/Ejerc-2.c
+++ TP2/Ejerc-2.c
@@ -9,38 +9,8 @@ void tomaCaracter (int contador);
 
 int main(int argc, char const *argv[])
 {
-   /* char caracter;
-    bool continuar;
-    int contador = 0 ;
-    
-    do
-    {
-            printf("         | Ingrese un caracter numerico: ");
-        scanf(" %c", &caracter);
-
-        if (esCaracterNumerico(caracter))
-        {
-            // printf("El caracter %c es numerico y su valor entero es %d\n", caracter, caracter - '0');
-            printf("-------  |\n");
-            printf("| q%d+ |  | El caracter %c es numerico y su valor entero es %d \n",contador,caracter, caracter - '0');
-            printf("-------  |\n");
-            printf("   |     |\n");
-            printf("   V     |\n");
-            continuar = true;
-            contador++;
-        }
-        else
-        {
-            //printf("El caracter %c no es numerico\n", caracter);
-            continuar = false;
-            printf("-------  |\n");
-            printf("|  q%d-|  | El caracter %c no es numerico\n",contador,caracter);
-            printf("-------  |\n");
-            }
-
-        
-    } while (continuar);*/
-    
+   
+ 
     tomaCaracter(0);
 
 
