96fca8eba9cb86b127a42ce78d22a24bdf9c5b26
MendioX <82616448+MendioX@users.noreply.github.com>
2023-09-29 17:05:45 -0300

    fix func tp2

--- TP2/Ejerc-2.c
+++ TP2/Ejerc-2.c
@@ -4,10 +4,12 @@
 
 bool esCaracterNumerico(char caracter);
 
+void tomaCaracter (int contador); 
+
 
 int main(int argc, char const *argv[])
 {
-    char caracter;
+   /* char caracter;
     bool continuar;
     int contador = 0 ;
     
@@ -37,10 +39,47 @@ int main(int argc, char const *argv[])
             }
 
         
-    } while (continuar);
+    } while (continuar);*/
+    
+    tomaCaracter(0);
+
 
     return 0;
 }
+
+
+void tomaCaracter (int contador){
+
+char caracter;
+    bool continuar;
+
+    printf("         | Ingrese un caracter numerico: ");
+    scanf(" %c", &caracter);
+
+    if (esCaracterNumerico(caracter))
+    {
+        printf("-------  |\n");
+        printf("| q%d+ |  | El caracter %c es numerico y su valor entero es %d\n", contador, caracter, caracter - '0');
+        printf("-------  |\n");
+        printf("   |     |\n");
+        printf("   V     |\n");
+        tomaCaracter(contador + 1);
+    }
+    else
+    {
+        continuar = false;
+        printf("-------  |\n");
+        printf("|  q%d-|  | El caracter %c no es numerico\n", contador, caracter);
+        printf("-------  |\n");
+    }
+    
+    if (continuar)
+    {
+        tomaCaracter(contador);
+    }
+
+}
+
 
 
 bool esCaracterNumerico(char caracter)
